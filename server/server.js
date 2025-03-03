b4be204c209bb3c15a7fb122f37bcb95ac745a5e
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 22:28:37 +0530

    update some codes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const http = require("http");
 
-const PORT = 3000;
-
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-
   if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Welcome to the Home Page!");
   } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("This is the About Page.");
   } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Contact us at contact@example.com.");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
@@ -17,6 +16,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
