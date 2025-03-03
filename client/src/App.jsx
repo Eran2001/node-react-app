ad551b4de4659c4bbddb4cc2619b8875968f7d4d
Eran <eran.hasarali.2001@gmail.com>
2025-03-03 22:46:18 +0530

    add something

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-const App = () => {
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/message")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h1 className="text-3xl font-bold underline">React + Node.js</h1>
+      <p>{message}</p>
     </div>
   );
-};
+}
 
 export default App;
